c364455179e3c591316b0c6fc1197ab87c941160
Drew Williams <williams.r.drew@gmail.com>
2018-02-10 14:03:14 -0500

    working on weather api and added google maps activity

--- Camping/app/src/main/java/com/example/sean/camping/MainActivity.java
+++ Camping/app/src/main/java/com/example/sean/camping/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.sean.camping;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, LocationChooser.class);
+        startActivity(intent);
+    }
+
+    public void getWeatherStuff() {
+        
     }
 }
